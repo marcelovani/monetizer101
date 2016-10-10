5d16b28348186bb3238d09a0d83d50f2f73b740d
Leon Kessler <leon.nk@gmail.com>
2016-10-10 16:37:49 +0100

    Case 26862: Switching to js widget implementation (and removing previous backend work)

--- templates/monetizer101-full.tpl.php
+++
@@ -1,6 +0,0 @@
-<div class="field-monetizer101-full" <?php print drupal_attributes($wrapper_attributes); ?>>
-  <?php if (!empty($table['#items'])): ?>
-    <h3>Best Offers</h3>
-    <?php print drupal_render($table); ?>
-  <?php endif; ?>
-</div>
--- templates/monetizer101-summary.tpl.php
+++
@@ -1,9 +0,0 @@
-<div class="field-monetizer101-summary">
-  <?php if (!empty($table['#items'])): ?>
-    <h3>Best Deals</h3>
-    <?php print drupal_render($table); ?>
-  <?php endif; ?>
-  <?php if ($view_all_link): ?>
-    <?php print $view_all_link; ?>
-  <?php endif; ?>
-</div>
