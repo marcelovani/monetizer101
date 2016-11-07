0e371a68d2e2f2d8269c1f5f65e915decc36495a
Will Howlett <will_howlett@dennis.co.uk>
2016-11-07 17:21:01 +0000

    Case 26988: make block sticky, not iframe. Improve logic

--- js/monetizer101Sticky.js
+++ js/monetizer101Sticky.js
@@ -5,7 +5,6 @@
     attach: function() {
       var sticky = {},
       stickyBlock = document.getElementById('block-monetizer101-monetizer101-sticky'),
-      iframe= '',
       height = 60,
       startPosition = 0,
       currentPosition = 0,
@@ -18,7 +17,7 @@
 
       // update required elements based on amount user has scrolled
       sticky.updateMove = function(amount) {
-        iframe.style.top = -height + amount + 'px';
+        stickyBlock.style.top = -height + amount + 'px';
         document.body.style.marginTop = amount + 'px';
         if (stickyNav) {
           if (amount >= 0) {
@@ -30,15 +29,20 @@
         }
       };
 
-      sticky.scrollMoveIframe = function() {
+      sticky.scrollMoveBlock = function() {
         // compare currentPosition & startPosition to see how far user has scrolled
         currentPosition = document.documentElement.scrollTop || document.body.scrollTop;
         moveAmount = (currentPosition - startPosition) / 2;
         // if user has scrolled more than height of sticky element fix the element and remove scroll event listener
         if (moveAmount >= height) {
           sticky.updateMove(height);
-          removeEventListener('scroll', sticky.scrollMoveIframe);
+          removeEventListener('scroll', sticky.scrollMoveBlock);
           document.body.classList.add('monetizer101-sticky');
+        }
+        // reset sticky trigger if user scrolls up before it is full shown
+        else if (moveAmount <= 0) {
+          startPosition = document.documentElement.scrollTop || document.body.scrollTop;
+          sticky.updateMove(0);
         }
         // otherwise update elements in sync with user's scroll
         else {
@@ -47,25 +51,22 @@
       };
 
       sticky.init = function() {
-        // hide wrapping block
-        stickyBlock.style.height = 0;
-        stickyBlock.style.margin = 0;
-        stickyBlock.style.overflow = 'hidden';
         // wait for iframe to load (we don't have access to a proper callback unfortunately)
         setTimeout(function () {
-          // set fixed position styles on iframe
-          iframe = stickyBlock.querySelector('iframe');
-          iframe.style.position = 'fixed';
-          iframe.style.top = -height + 'px';
-          iframe.style.left = 0;
-          iframe.style.right = 0;
-          iframe.style.width = '100%';
-          iframe.style.height = height + 'px';
-          iframe.style.zIndex = 500;
-          iframe.style.backgroundColor = 'white';
+          // set fixed position styles on block
+          stickyBlock.style.overflow = 'hidden';
+          stickyBlock.style.position = 'fixed';
+          stickyBlock.style.top = -height + 'px';
+          stickyBlock.style.left = 0;
+          stickyBlock.style.right = 0;
+          stickyBlock.style.width = '100%';
+          stickyBlock.style.height = height + 'px';
+          stickyBlock.style.margin = 0;
+          stickyBlock.style.zIndex = 500;
+          stickyBlock.style.backgroundColor = 'white';
           // set start position & add scroll event listener
           startPosition = document.documentElement.scrollTop || document.body.scrollTop;
-          addEventListener('scroll', sticky.scrollMoveIframe);
+          addEventListener('scroll', sticky.scrollMoveBlock);
         }, 5000);
       };
 
